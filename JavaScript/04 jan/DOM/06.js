948a0f4c889866659e3edfe770afc26d5575aac3
vishal-038 <vk067414@gmail.com>
2024-01-11 15:48:55 +0530

    "11th jan second half"

--- JavaScript/04 jan/DOM/06.js
+++ JavaScript/04 jan/DOM/06.js
@@ -5,7 +5,7 @@
 //for of loop
 const navItem2 = document.querySelectorAll(".nav-item");
 console.log("..................");
-for(let i of navItem2){
-    console.log(i.textContent);
-    i.style.color="red";
+for (let i of navItem2) {
+  console.log(i.textContent);
+  i.style.color = "red";
 }
